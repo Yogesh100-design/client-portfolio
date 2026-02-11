5a226d797f553f3fd316d03a6ceb9f1329f2540c
Yogesh chavan <yogeshchavan1209@gmail.com>
2026-02-11 23:38:01 +0530

    add code base

---
+++ src/data/projects.js
@@ -0,0 +1,31 @@
+export const projects = [
+    {
+    id: 1,
+    title: "Unisire",
+    description: "India’s first student-focused learning and career assistance platform combining structured roadmaps, resume references, and automation tools. Features a sitemap-driven chatbot for enhanced navigation.",
+    techStack: ["React.js", "Tailwind CSS", "Python", "Django", "SQL"],
+    github: "https://github.com/unisiremainhub-gif/Unisire", 
+    live: "https://www.unisire.com/", 
+    image: "../assets/images/unisire.png" 
+  },
+  {
+    id: 2,
+    title: "YouTube Bookmark Extension",
+    description: "A Chrome browser extension that enables users to save and revisit specific timestamps in YouTube videos, improving learning and content navigation.",
+    techStack: ["JavaScript", "HTML", "CSS", "Chrome Extension API"],
+    github: "https://github.com/Nitinkeldee/YouTube-Bookmark-Extension", 
+    live: "", 
+    image: "../assets/images/YCE.png" 
+  },
+  {
+    id: 3,
+    title: "EduMedia",
+    description: "A digital learning platform featuring study materials, interactive UI, subjects, videos, and notes.",
+    techStack: ["React", "Node.js", "Express", "MongoDB"],
+    github: "https://github.com/Nitin100-design/EduMedia-Hub",
+    live: "https://edumedia-hub-2.onrender.com",
+    image: "../assets/images/Edumedia.jpg"
+  },
+];
+
+export default projects;
